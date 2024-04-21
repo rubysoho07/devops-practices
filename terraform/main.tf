782f4da71c584c13737c5ab8ac05ac6e27bc4958
Yungon Park <hahafree12@gmail.com>
2024-04-21 20:52:39 +0900

    Control Node 추가

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,12 +12,25 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-# resource "aws_instance" "managed_node" {
-#   ami = data.aws_ssm_parameter.amazon_linux_2023_ami.value
-#   instance_type = "t3.micro"
-# }
+resource "aws_instance" "control_node" {
+  ami = data.aws_ssm_parameter.amazon_linux_2023_ami.value
+  instance_type = "t3.micro"
+  subnet_id = data.aws_subnets.public.ids[0]
+  vpc_security_group_ids = [ var.security_group_id ]
+  key_name = var.ssh_key_name
 
-resource "aws_instance" "inventory_nodes" {
+  user_data = <<EOF
+#!/bin/bash
+sudo dnf update -y
+sudo dnf install ansible -y
+EOF
+
+  tags = {
+    Name = "Ansible-Control-Node"
+  }
+}
+
+resource "aws_instance" "managed_nodes" {
   count = 3
   ami = data.aws_ssm_parameter.amazon_linux_2023_ami.value
   instance_type = "t3.micro"
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
+output "control_node_ip" {
+  value = aws_instance.control_node.public_ip
+}
+
 output "managed_node_ips" {
-  value = [for instance in aws_instance.inventory_nodes : instance.public_ip]
+  value = [for instance in aws_instance.managed_nodes : instance.public_ip]
 }
