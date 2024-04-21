9619ab7fde1e4694910d062e60ceed5831e448b0
Yungon Park <hahafree12@gmail.com>
2024-04-21 19:05:43 +0900

    Add key_name argument on EC2 instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,6 +23,7 @@ resource "aws_instance" "inventory_nodes" {
   instance_type = "t3.micro"
   subnet_id = data.aws_subnets.public.ids[count.index % length(data.aws_subnets.public.ids)]
   vpc_security_group_ids = [ var.security_group_id ]
+  key_name = var.ssh_key_name
 
   tags = {
     Name = "Ansible-Test-${count.index}"
