7cb9b395b42ca8fc68e6021fef922a6c3ea68c22
Yungon Park <hahafree12@gmail.com>
2025-12-14 21:39:06 +0900

    NAT Instance 스크립트를 User Data에서 관리하도록 변경

--- examples/create_nat_instance/main.tf
+++ examples/create_nat_instance/main.tf
@@ -73,6 +73,25 @@ resource "aws_instance" "nat_instance" {
 
   source_dest_check = false
 
+  user_data = <<EOF
+#!/bin/bash
+dnf install -y iptables-services
+systemctl enable --now iptables-services
+
+# Kernel Setting
+echo "net.ipv4.ip_forward = 1" | sudo tee -a /etc/sysctl.conf
+sysctl -p /etc/sysctl.conf
+
+# iptables rule
+iptables -t nat -A POSTROUTING -o ens5 -s 0.0.0.0/0 -j MASQUERADE
+iptables -F FORWARD
+service iptables save
+EOF
+
+  lifecycle {
+    ignore_changes = [ami, user_data]
+  }
+
   tags = {
     Name = "NAT-Instance"
   }
@@ -84,3 +103,21 @@ resource "aws_eip_association" "nat_instance_eip_assoc" {
   allocation_id = aws_eip.nat_instance_eip.id
 }
 
+# Outputs
+output "nat_instance_id" {
+  description = "The ID of the NAT instance"
+  value       = aws_instance.nat_instance.id
+}
+
+/*
+Note: How To add the route with AWS CLI
+(1) Retrieve the route table (from tag) and the NAT instance id
+export INSTANCE_ID=$(terraform output -raw nat_instance_id)
+export ROUTE_TABLE_ID=$(aws ec2 describe-route-tables --filters Name=tag:Name,Values=YOUR_TAG --query "RouteTables[0].RouteTableId" --output text)
+
+(2) Create a route for "0.0.0.0/0"
+aws ec2 create-route --route-table-id $ROUTE_TABLE_ID --instance-id $INSTANCE_ID --destination-cidr-block "0.0.0.0/0"
+
+(3) Delete the route for "0.0.0.0/0"
+aws ec2 delete-route --destination-cidr-block "0.0.0.0/0" --route-table-id $ROUTE_TABLE_ID
+*/
