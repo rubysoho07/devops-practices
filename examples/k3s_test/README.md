9ecf60e4c23d405a5787cf3cfc6c219046876f2f
Yungon Park <hahafree12@gmail.com>
2026-02-03 22:28:12 +0900

    K3s HA Server 구성

--- examples/k3s_test/README.md
+++ examples/k3s_test/README.md
@@ -53,3 +53,44 @@ ansible-inventory -i inventory_k3s_multi_aws_ec2.yml --graph
 # Playbook 실행
 ansible-playbook -i inventory_k3s_multi_aws_ec2.yml -u ec2-user --private-key PRIVATE_KEY_PATH playbook-k3s-multi.yaml
 ```
+
+## 고가용성 서버 구성 (서버 3)
+
+서버 3대로 고가용성을 추구하고자 합니다. 이 때 서버는 Control Plane 겸 Data Plane의 역할을 수행합니다. (데이터는 내장 etcd로 관리합니다)
+
+**Terraform 코드 구성**
+
+다음 리소스만으로 구성합니다. (나머지는 주석 처리해 주세요)
+- 보안 그룹 (k3s_nodes) 및 관련 규칙
+- aws_instance.k3s_ha_server EC2 인스턴스
+- LB, Target Group, Target Group Attachment
+
+배포 방법
+
+```shell
+terraform init
+terraform apply
+
+# nslookup으로 NLB의 공인 IP 주소를 확인하여 메모합니다.
+```
+
+**Ansible로 인스턴스 설정**
+
+```shell
+# Inventory 확인
+ansible-inventory -i inventory_k3s_ha_aws_ec2.yml --graph
+
+# Playbook 실행
+# (YOUR_RANDOM_TOKEN 값은 임의의 값으로 생성합니다)
+ansible-playbook -i inventory_k3s_ha_aws_ec2.yml -u ec2-user \
+--private-key PRIVATE_KEY_PATH \
+-e nlb_public_ip=NLB_PUBLIC_IP \
+-e k3s_token=YOUR_RANDOM_TOKEN \
+playbook-k3s-ha.yaml
+```
+
+## 리소스 정리
+
+```shell
+terraform destroy
+```
--- examples/k3s_test/main.tf
+++ examples/k3s_test/main.tf
@@ -18,6 +18,13 @@ data "aws_subnet" "public_subnet" {
   }
 }
 
+data "aws_subnet" "public_subnet_c" {
+  filter {
+    name   = "tag:Name"
+    values = ["PUBLIC-2A"]
+  }
+}
+
 data "aws_ssm_parameter" "amazon_linux_2023_ami" {
   name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-6.1-arm64"
 }
@@ -125,4 +132,57 @@ resource "aws_instance" "k3s_multi_agent" {
   tags = {
     Name = "k3s_multi_agent"
   }
+}
+
+resource "aws_instance" "k3s_ha_server" {
+  count                  = 3
+  ami                    = data.aws_ssm_parameter.amazon_linux_2023_ami.value
+  instance_type          = "t4g.small"
+  subnet_id              = data.aws_subnet.public_subnet.id
+  key_name               = "your_key_pair"
+  vpc_security_group_ids = [aws_security_group.k3s_nodes.id]
+
+  tags = {
+    Name  = "k3s-ha-server-${count.index}"
+    Group = "k3s"
+  }
+}
+
+resource "aws_lb_target_group" "k3s_ha_server" {
+  name     = "k3s-ha-server"
+  port     = 6443
+  protocol = "TCP"
+  vpc_id   = data.aws_vpc.default.id
+}
+
+resource "aws_lb_target_group_attachment" "k3s_ha_server" {
+  count            = 3
+  target_group_arn = aws_lb_target_group.k3s_ha_server.arn
+  target_id        = aws_instance.k3s_ha_server[count.index].id
+  port             = 6443
+}
+
+resource "aws_lb" "k3s_ha_nlb" {
+  name               = "k3s-ha-nlb"
+  internal           = false
+  load_balancer_type = "network"
+  subnets            = [data.aws_subnet.public_subnet.id, data.aws_subnet.public_subnet_c.id]
+
+  tags = {
+    Name = "k3s-ha-nlb"
+  }
+}
+
+resource "aws_lb_listener" "k3s_ha_nlb_6443" {
+  load_balancer_arn = aws_lb.k3s_ha_nlb.arn
+  port              = 6443
+  protocol          = "TCP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.k3s_ha_server.arn
+  }
+}
+
+output "k3s_ha_nlb_dns_name" {
+  value = aws_lb.k3s_ha_nlb.dns_name
 }
